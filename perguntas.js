1128cb048aad56f48c285378df345c8af5438d2c
Ricardo Tataruk <ricardo.tataruk.rosa@escola.pr.gov.br>
2025-10-15 00:25:27 +0000

    finalizado

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos EUA?',
+    'A capital dos EUA é washington'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é uma silaba?',
+    'é um fonema ou grupo de fonemas que se pronuncia em uma única emissão de voz,.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amigo em Inglês?',
+    'amigo em ingles é FRIEND'
+)
+
+criaCartao(
+    'biologia',
+    'Oque é uma célula procarinte?',
+    'é um tipo de célula simples e primitiva que não possui um núcleo delimitado por membrana e nem outras organelas membranosas'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvarez Cabral'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 5 x 2?',
+    '5 x 2 é igual a 10'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'Oque é CDB?',
+    'Certificado de depósito bancário'
+)
+
+criaCartao(
+    'Física',
+    'Oque significa a sigla J?',
+    'Jounne'
+)
+
+
+
+
+
+
+
